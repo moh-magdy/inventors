61b3a69a58a0f71f4dea13ed5dc92a0c68dc7ef0
moh-magdy <moh.mgd@gmail.com>
2017-11-05 22:30:37 +0200

    nav-bar

--- includes/templates/navbar.php
+++ includes/templates/navbar.php
@@ -5,4 +5,13 @@
 =================================================================================
 */
 ?>
-<h1>Nav Bar</h1>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>

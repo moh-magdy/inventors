d2eb6b2c5c8022f61e8d0973514500e3c931c063
moh-magdy <moh.mgd@gmail.com>
2017-11-05 21:04:32 +0200

    materialize

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -13,6 +13,7 @@
       <script src="<?php echo $js ?>jquery-3.2.1.min.js"></script>
       <script src="<?php echo $js ?>html5shiv.js"></script>
       <script src="<?php echo $js ?>respond.min.js"></script>
+      <script src="<?php echo $js ?>materialize.min.js"></script>
       <script src="<?php echo $js ?>plugins.js"></script>
     </body>
 </html>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -7,7 +7,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="ar">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <!-- ie -->
@@ -15,7 +15,11 @@
     <!-- device-width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Title Name</title>
-    <link rel="stylesheet" href="<?php echo $css; ?>style.css" />
+    <!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+    <!--[if lt IE 9]>
+    <script src="layout/js/html5shiv.min.js"></script>
+    <script src="layout/js/js/respond.min.js"></script>
+    <![endif]-->
   </head>
 
     <body>

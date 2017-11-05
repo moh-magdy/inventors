a14121e73d5c5f7dc0f3dc076dd4e1603daade08
Kemal-Helios <kamalhelios1@gmail.com>
2017-11-05 23:05:24 +0300

    Good

--- Admin/includes/templates/footer.php
+++ Admin/includes/templates/footer.php
@@ -10,6 +10,7 @@
                 <h1>Footer</h1>
             </div>   
       <script src="<?php echo $js ?>jquery-3.2.1.min.js"></script>
+      <script src="<?php echo $js ?>materialize.min.js"></script>
       <script src="<?php echo $js ?>html5shiv.js"></script>
       <script src="<?php echo $js ?>respond.min.js"></script>
       <script src="<?php echo $js ?>materialize.min.js"></script>
--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -11,6 +11,7 @@
       </div>
     </div>
       <script src="<?php echo $js ?>jquery-3.2.1.min.js"></script>
+      <script src="<?php echo $js ?>materialize.min.js"></script>
       <script src="<?php echo $js ?>html5shiv.js"></script>
       <script src="<?php echo $js ?>respond.min.js"></script>
       <script src="<?php echo $js ?>materialize.min.js"></script>

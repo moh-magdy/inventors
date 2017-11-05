0f5ba93f6eb1f3c792abfbbab8959ff406407f32
Kemal-Helios <kamalhelios1@gmail.com>
2017-11-05 23:08:01 +0300

    s

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -9,17 +9,18 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
     <!-- ie -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- device-width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Title Name</title>
+    <link rel="stylesheet" href="<?php echo $css ?>materialize.min.css" />
+    <link rel="stylesheet" href="<?php echo $css ?>style.css" />
     <!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
     <!--[if lt IE 9]>
     <script src="layout/js/html5shiv.min.js"></script>
     <script src="layout/js/js/respond.min.js"></script>
-    <![endif]-->
   </head>
 
     <body>

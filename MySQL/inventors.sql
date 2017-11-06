f7878e6113194389eb511ca4220d4ebfa6769e1c
Kemal-Helios <kamalhelios1@gmail.com>
2017-11-06 23:31:12 +0300

    good

--- MySQL/inventors.sql
+++ MySQL/inventors.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2017 at 09:19 PM
+-- Generation Time: Nov 06, 2017 at 09:29 PM
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -21,6 +21,159 @@ SET time_zone = "+00:00";
 --
 -- Database: `inventors`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `Ordering` int(11) DEFAULT NULL,
+  `Visibility` tinyint(4) NOT NULL DEFAULT '0',
+  `Allow_Comment` tinyint(4) NOT NULL DEFAULT '0',
+  `Allow_Ads` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `c_id` int(11) NOT NULL,
+  `Comment` text NOT NULL,
+  `Status` tinyint(4) NOT NULL,
+  `Comment_date` date NOT NULL,
+  `Item_id` int(11) NOT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `Add_Date` datetime NOT NULL,
+  `Country_Mad` varchar(255) NOT NULL,
+  `Images` varchar(255) NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  `Rating` smallint(6) NOT NULL,
+  `Approve` tinyint(4) NOT NULL DEFAULT '0',
+  `Cat_ID` int(11) NOT NULL,
+  `Member_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL COMMENT 'To identify User',
+  `UserName` varchar(255) NOT NULL COMMENT 'To Username login',
+  `Password` varchar(255) NOT NULL COMMENT 'To Password login',
+  `Email` varchar(255) NOT NULL,
+  `GroupID` int(11) NOT NULL DEFAULT '0' COMMENT 'Identify User Group',
+  `RegStatus` int(11) NOT NULL DEFAULT '0' COMMENT 'User Approval',
+  `dateNow` date NOT NULL,
+  `Avatar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `UserName`, `Password`, `Email`, `GroupID`, `RegStatus`, `dateNow`, `Avatar`) VALUES
+(1, 'Kamal', '601f1889667efaebb33b8c12572835da3f027f78', 'Kamalhelios1@gmail.com', 1, 1, '2017-07-01', ''),
+(21, 'mohmed', '048e56df20a3216fc31306b65e325714ad235616', 'moh.mgd@gmail.com', 1, 0, '2017-11-06', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Name` (`Name`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`c_id`),
+  ADD KEY `item_comment` (`Item_id`),
+  ADD KEY `USER_comment` (`User_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `member_1` (`Member_ID`),
+  ADD KEY `cat_1` (`Cat_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'To identify User', AUTO_INCREMENT=23;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `USER_comment` FOREIGN KEY (`User_id`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_comment` FOREIGN KEY (`Item_id`) REFERENCES `items` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `cat_1` FOREIGN KEY (`Cat_ID`) REFERENCES `categories` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `member_1` FOREIGN KEY (`Member_ID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
